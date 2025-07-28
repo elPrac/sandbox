f3ea3183caf040d1ec064ee89876e88715ef6457
Miguel Miranda <mmikernel@gmail.com>
2025-07-28 15:47:41 -0600

    scripts: add Yocto support alongside Buildroot in Docker toolchain
    Extend the existing Docker-based build infrastructure to support
    Yocto in addition to Buildroot. This includes:
    
    - Addition of relevant Yocto submodules:
      - bitbake
      - meta-openembedded
      - meta-st-stm32mp
      - meta-st-openstlinux
      - meta-qt5
      - openembedded-core
    
    - Dockerfile updated to support both build environments via ARGs.
      Package installation is now conditional on BUILD_ENV (buildroot/yocto),
      and base image is configurable through BASE_IMAGE.
    
    - build.sh updated to accept --env argument and select appropriate base image
      and image tags. Defaults to Buildroot.
    
    - start.sh updated to:
      - propagate selected environment
      - source appropriate build.sh logic
      - invoke Yocto init environment (oe-init-build-env) as default shell
        when Yocto is selected
    
    This refactor enables easier switching between embedded build systems
    and reduces duplication across Dockerfiles and scripts.
    
    Signed-off-by: Miguel Miranda <mmikernel@gmail.com>

--- scripts/build-docker/build.sh
+++ scripts/build-docker/build.sh
@@ -1,11 +1,44 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
-docker_image=build/linux-mentee:buildroot
-# Look if there's already an img for us
-if [[ "$(docker images -q $docker_image 2> /dev/null)" == "" ]]; then
-    # If not build it
-	pushd $SCRIPT_DIR/build-docker
-	docker build -t $docker_image .
-	popd
+
+env="${env:-buildroot}"
+
+# Accept --env <value> or just a positional <value>
+# meaning you can call build.sh either way:
+# ./build.sh yocto      # Yocto
+# ./build.sh buildroot  # Buildroot
+
+if [[ $# -ge 1 ]]; then
+    if [[ "$1" == "-e" || "$1" == "--env" ]]; then
+        env="${2:-}"
+        shift 2
+    else
+        env="$1"
+        shift
+    fi
 fi
 
+case "$env" in
+    buildroot)
+        docker_image="build/linux-sandbox:buildroot"
+        base_image="debian:bookworm-slim"
+        ;;
+    yocto)
+        docker_image="build/linux-sandbox:yocto"
+        base_image="crops/poky:ubuntu-22.04"
+        ;;
+    *)
+        echo "Usage: $0 [--env <buildroot|yocto>] OR $0 <buildroot|yocto>"
+        exit 1
+        ;;
+esac
+
+# Export for sourcing in start.sh
+export docker_image
+
+# Only build image if missing
+if [[ -z "$(docker images -q "$docker_image" 2>/dev/null)" ]]; then
+    pushd "$SCRIPT_DIR/build-docker" > /dev/null
+    docker build --build-arg BASE_IMAGE="$base_image" -t "$docker_image" .
+    popd > /dev/null
+fi
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,27 +1,61 @@
 #!/usr/bin/env bash
 set -o errexit -o nounset -o pipefail -o errtrace
-# set -x
+set -x
+
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-readonly root_dir="$(readlink -f $SCRIPT_DIR/../)"
+readonly root_dir="$(readlink -f "$SCRIPT_DIR/../")"
+readonly WORK_DIR="/workdir"
+readonly OEROOT="$WORK_DIR/sources/openembedded-core"
+readonly TEMPLATECONF="$WORK_DIR/sources/openembedded-core/meta/conf/templates/default"
+readonly BUILDDIR="$WORK_DIR/build_sandbox"
 
-. $SCRIPT_DIR/build-docker/build.sh
+# Add 
+OE_INIT_BUILD_CMD="source $OEROOT/oe-init-build-env $BUILDDIR"
+OE_BB_INIT_CMD="$OE_INIT_BUILD_CMD && bash"
+env="${env:-buildroot}"
+cmd_args=()
 
+# Parse environment selection only
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -e|--env)
+            env="$2"
+            shift 2
+            ;;
+        -h|--help)
+            echo "Usage: $0 [-e|--env <buildroot|yocto>] [command]"
+            exit 0
+            ;;
+        *)
+            cmd_args+=("$1")
+            shift
+            ;;
+    esac
+done
 
-# TODO:
-# Docker opt needed for bridged net-stack
-#--device=/dev/vhost-net
-#--device-cgroup-rule='c *:* rwm'
-#-p 80:8080/tcp
-#-p 2222:22/tcp
-#--cap-add NET_ADMIN
+# Call build.sh with --env and source it to get docker_image
+. $SCRIPT_DIR/build-docker/build.sh --env "$env"
 
-cmd=${@:-"bash"}
+cmd="${cmd_args[*]:-$OE_BB_INIT_CMD}"
+
+# Docker opts
 docker_opts=(
     --rm
     --interactive
     --tty
-    --volume ${root_dir}:/workdir
-    $docker_image
+    --env OEROOT=$OEROOT
+    --env TEMPLATECONF=$TEMPLATECONF
+    --volume "$root_dir:$WORK_DIR"
+    --workdir $WORK_DIR
+    "$docker_image"
     bash -c "$cmd"
 )
-docker run ${docker_opts[@]}
+
+# TODO:
+# --device=/dev/vhost-net
+# --device-cgroup-rule='c *:* rwm'
+# -p 80:8080/tcp
+# -p 2222:22/tcp
+# --cap-add NET_ADMIN
+
+docker run "${docker_opts[@]}"
