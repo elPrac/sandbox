c30e7a10a30f9268cdcaf8c4fe31ebaed7a17a34
Miguel Miranda <mmikernel@gmail.com>
2025-09-14 12:34:10 -0600

    buildroot-external-syzbot: refine rsync exclusions and add debug info
    Signed-off-by: Miguel Miranda <mmikernel@gmail.com>

--- buildroot-external-syzbot/override.mk
+++ buildroot-external-syzbot/override.mk
@@ -1,3 +1,10 @@
 LINUX_OVERRIDE_SRCDIR=$(TOPDIR)/../linux-mainline/
 #SYZKALLER_OVERRIDE_SRCDIR=$(TOPDIR)/../syzkaller/
-LINUX_OVERRIDE_SRCDIR_RSYNC_EXCLUSIONS=--include .git
+#LINUX_OVERRIDE_SRCDIR_RSYNC_EXCLUSIONS=--include .git
+LINUX_OVERRIDE_SRCDIR_RSYNC_EXCLUSIONS += \
+  --exclude .cache --exclude .clangd --exclude .vscode \
+  --exclude '.*.cmd' --exclude '*.o' --exclude '*.a' --exclude '*.ko' \
+  --exclude 'kout-*' --exclude 'build-*'
+
+$(info [BR-EXT] BR2_EXTERNAL=$(BR2_EXTERNAL))
+$(info [BR-EXT] LINUX_OVERRIDE_SRCDIR=$(LINUX_OVERRIDE_SRCDIR))
